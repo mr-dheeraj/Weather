38ab6cd718f2ec52fcd54c4edba188d2d0bf04c1
mr-dheeraj <72618058+mr-dheeraj@users.noreply.github.com>
2021-10-01 14:04:50 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter import font
+import requests
+HEIGHT=500
+WIDTH=600
+
+def test_function(entry):
+	print("this is an entry !",entry)
+#8628c6b6f3044eeb0dc8b7dc3cab39ff
+def format_response(weather):
+	try:
+		name = weather['name']
+		desc=weather['weather'][0]['description']
+		temp=weather['main']['temp']
+		final_str='City: %s \nConditions: %s \nTemperature(°F):%s '%(name,desc,temp)
+	    
+	    
+	except:
+		final_str='there is a problem'
+
+
+	return final_str
+
+def get_weather(city):
+	weather_key = '8628c6b6f3044eeb0dc8b7dc3cab39ff'
+	url='https://api.openweathermap.org/data/2.5/weather'
+	params={'APPID': weather_key, 'q':city, 'units':'imperial'}
+	response=requests.get(url,params=params)
+	weather=response.json()
+
+	label['text']=format_response(weather)
+
+
+
+root = tk.Tk()
+canvas=tk.Canvas(root,height=HEIGHT,width=WIDTH)
+canvas.pack()
+
+background_image=tk.PhotoImage(file='sky.png')
+background_label=tk.Label(root,image=background_image)
+background_label.place(relwidth=1,relheight=1)
+
+frame=tk.Frame(root,bg='#80c1ff',bd=5)
+frame.place(relx=0.5,rely=0.1,relwidth=0.75,relheight=0.1,anchor='n')
+
+entry=tk.Entry(frame,font=('Courier',18))
+entry.place(relwidth=0.65,relheight=1)
+
+button=tk.Button(frame,text="Get Weather",font=('Courier',12),command=lambda: get_weather(entry.get()))
+button.place(relx=0.7,relheight=1,relwidth=0.3)
+
+lower_frame=tk.Frame(root,bg='#80c1ff',bd=10)
+lower_frame.place(relx=0.5,rely=0.25,relwidth=0.75,relheight=0.6,anchor='n')
+
+label=tk.Label(lower_frame,font=('Courier',18),bd=4)
+label.place(relwidth=1,relheight=1)
+root.mainloop()
+
+
